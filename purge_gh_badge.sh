1e5048d097e9a66881f38964713b0015ad1282d0
Justin Forsyth <justin@forsyth.com>
2025-03-25 14:18:39 -0400

    update release pipeline to purge camo badge cache

--- purge_gh_badge.sh
+++ purge_gh_badge.sh
@@ -2,4 +2,6 @@
 
 # See: https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/about-anonymized-urls#removing-an-image-from-camos-cache
 # Badge host has 12 hour cache: https://github.com/softwaremill/maven-badges/issues/445
+# This must update first: https://maven-badges.herokuapp.com/sonatype-central/com.forsyth.novm/novm-runtime/badge.svg
+# Then, this script can be run to purge Camo
 curl -X PURGE $NOVM_GH_BADGE_URL
